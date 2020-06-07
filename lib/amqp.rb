d94ccf8edb5449292a764c8fa78374aa2755fd7c
liam <liamyabou@outlook.com>
2020-06-07 12:49:52 +0800

    Revise the variables and the logic.

--- lib/amqp.rb
+++ lib/amqp.rb
@@ -1,15 +1,33 @@
 module AMQP
     class Client
-        attr_accessor :connection, :channel, :exchange, :server_queue_name, :correlation_id, :lock,
-                      :condition, :reply_queue, :response
+        def initialize
+            @connection = Bunny.new
+            connection.start
+            @channel = connection.create_channel
+        end
 
-        def initialize(server_queue_name)
-           @connection = Bunny.new
-           @connection.start
-           @channel = connection.create_channel
-           @exchange = channel.default_exchange
-           @server_queue_name = server_queue_name
-           setup_reply_queue
+         # params e.g.,
+         # { server_queue_name: SERVER_QUEUE_NAME, category_id: category_id, page: page }
+        def rpc(action, **params)
+            exchange = channel.default_exchange
+            mutex = Mutex.new
+            condition = ConditionVariable.new
+            reply_queue = channel.queue("", exclusive: true)
+            response = nil
+            reply_queue.subscribe do |_delivery_info, properties, payload|
+                 # decode the json from the queue
+                 response = JSON.parse(payload)
+                 # sends the signal to continue the execution of #call
+                 mutex.synchronize { condition.signal }
+            end
+            correlation_id = generate_uuid
+            message = "#{action}/#{params[:category_id].to_s}/#{params[:page]}"
+            options = { routing_key:  params[:server_queue_name], correlation_id: correlation_id,
+                        reply_to: reply_queue.name, content_type:  "application/json" }
+            exchange.publish(message, options)
+             # wait for the signal to continue the execution
+             mutex.synchronize { condition.wait(mutex) }
+             response
         end
 
         def stop
@@ -17,44 +35,13 @@ module AMQP
             connection.close
         end
 
-        # params e.g., {action: "find_products", category_id: 2, page: 1}
-        def call(**params)
-            @correlation_id = generate_uuid
-            puts "correlation_id: #{correlation_id}"
-            message = "#{params[:action]}/#{params[:category_id].to_s}/#{params[:page]}"
-            puts "message: #{message}"
-            puts "reply_queue: #{reply_queue.name}"
-            exchange.publish(message, 
-                             routing_key: server_queue_name, 
-                             correlation_id: correlation_id,
-                             reply_to: reply_queue.name,
-                             content_type: "application/json")
-            # wait for the signal to continue the execution
-            lock.synchronize { condition.wait(lock) }
-            response
-        end
-
         private
-
-        def setup_reply_queue
-            @lock = Mutex.new
-            @condition = ConditionVariable.new
-            that = self
-            @reply_queue = channel.queue("", exclusive: true)
-            reply_queue.subscribe do |_delivery_info, properties, payload|
-                # debug
-                if properties[:correlation_id] == that.correlation_id
-                    # decode the json from the queue
-                    that.response = JSON.parse(payload)
-                    # sends the signal to continue the execution of #call
-                    that.lock.synchronize { that.condition.signal }
-                end
-            end
-        end
 
         def generate_uuid
             "#{rand}#{rand}#{rand}"
         end
+
+        attr_reader :connection, :channel
     end
 end
 
--- lib/top100_ranking.rb
+++ lib/top100_ranking.rb
@@ -4,16 +4,14 @@ module Top100Ranking
     SERVER_QUEUE_NAME = "rpc_queue".freeze
 
     class Client
-        attr_accessor :client
-
-        def initialize(client: ::AMQP::Client.new(SERVER_QUEUE_NAME))
+        def initialize(client: ::AMQP::Client.new)
             @client = client
         end
 
         def find_products(category_id:, page: 1)
-            params = { action: "find_products", category_id: category_id, page: page }
-            response = client.call(params)
-            puts "response: #{response}"
+            params = { server_queue_name: SERVER_QUEUE_NAME, 
+                       category_id: category_id, page: page }
+            response = client.rpc("find_products", params)
             finalize_amqp
             response
         end
@@ -23,6 +21,8 @@ module Top100Ranking
         def finalize_amqp
             client.stop
         end
+
+        attr_reader :client
     end
 end
 
