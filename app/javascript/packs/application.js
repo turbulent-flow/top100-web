2d22a1eabd6f94f5db01b8bf61085152607052b7
liam <liamyabou@outlook.com>
2020-06-11 17:34:28 +0800

    load turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,11 +20,10 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
     $("#slidebarCollapse").on("click", function() {
         $("#slidebar").toggleClass("active")
         $("#slidebarCollapse .menu-icon").toggleClass("active")
         $("#webTitle").toggleClass("active")
     });
-});
+}); 
