5ab163827bd34408fa7d2aad0868f8530efbc179
liam <liamyabou@outlook.com>
2020-07-06 17:49:12 +0800

    Add the `CLOUDFRONT_ASSETS_ENDPOINT` variable for the frontend asstes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-    def customize_image_tag(name)
-        if ENV["RACK_ENV"] == "development"
-            image_tag("#{ENV["CLOUDFRONT_ENDPOINT"]}/#{name}")
-        else
-            image_tag(name)
-        end
-    end
 end
